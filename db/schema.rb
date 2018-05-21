88381f0faccdba1178b77fddf677bd17e10861aa
Hung Phan <hungpv@atmarkcafe.org>
2018-05-21 17:22:20 +0700

    add all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_081831) do
+ActiveRecord::Schema.define(version: 2018_05_21_082606) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -23,10 +26,9 @@ ActiveRecord::Schema.define(version: 2018_05_21_081831) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 10, scale: 2
     t.boolean "published"
-    t.string "shprt_description"
-    t.integer "category_id"
+    t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
